0210fee7cddfce8162bcc82e8c2b9bb1776d041c
InseonYu <42953720+InseonYu@users.noreply.github.com>
2018-11-19 22:55:05 -0600

    add direction
    add direction

--- js/iyumap.js
+++ js/iyumap.js
@@ -1,9 +1,13 @@
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
+  var home1 = {lat: 37.4988072, lng : 126.5327613};
+  var home2 = {lat: 36.7746063, lng : 126.4661203};
+  var home3 = {lat: 36.6290121, lng : 127.4552537};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {zoom: 4, center: home1});
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  var marker1 = new google.maps.Marker({position: home1, map: map});
+  var marker2 = new google.maps.Marker({position: home2, map: map});
+  var marker3 = new google.maps.Marker({position: home3, map: map});
 }
